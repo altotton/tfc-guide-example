6ef466cab5154a58aeb47d042ac7119845875de3
altotton <70298538+altotton@users.noreply.github.com>
2020-09-28 15:14:37 +0100

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -3,17 +3,12 @@ variable "aws_region" {
   default = "us-west-1"
 }
 
-variable "db_table_name" {
+variable "secret_key" {
   type    = string
-  default = "terraform-learn"
+  default = "xJ6V1NGQ2QtMQecr8vA844Cp5fFgLSiehRw4Trz1"
 }
 
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
+variable "access_key" {
+  type    = string
+  default = "AKIAJN2O7N6WZI2Z2FGQ"
 }
